7b6ec2c83495e9c87812f432e5dfa0315a393052
Conor Colgan <conorcolgan10@gmail.com>
2017-02-27 17:38:39 +0000

    Initial Project Commit
    Started some work on the script, and set up an example to debug it. Async progress seems erratic.

---
+++ Loading Bar/Assets/Example/ExampleScript.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Threading;
+
+public class ExampleScript : MonoBehaviour {
+
+    [SerializeField]
+    GameObject LoadingBar;
+
+
+    // Use this for initialization
+    void Start ()
+    {
+        if (SceneManager.GetActiveScene().name == "LoadingScene")
+        {
+            LoadNextScene();
+        }
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (LoadingBar != null)
+        {
+            LoadingBarProgress p = GetComponent<LoadingBarProgress>();
+            LoadingBar.transform.localScale = new Vector3( p.progress, LoadingBar.transform.localScale.y, LoadingBar.transform.localScale.z);
+        }
+	}
+
+    public void LoadLoadingScreen()
+    {
+        SceneManager.LoadScene("LoadingScene");
+    }
+
+    public void LoadNextScene()
+    {
+        LoadingBarProgress l = GetComponent<LoadingBarProgress>();
+        StartCoroutine(l.AsynchronousLoad(SceneManager.LoadSceneAsync("Scene2")));
+    }
+}
---
+++ Loading Bar/Assets/LoadingBarProgress.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadingBarProgress : MonoBehaviour {
+
+    public float progress;
+
+public IEnumerator AsynchronousLoad(AsyncOperation ao)
+    {
+        while (!ao.isDone)
+        {
+            progress = ao.progress;
+            Debug.Log(progress);
+            yield return null;
+        }
+    }
+}
